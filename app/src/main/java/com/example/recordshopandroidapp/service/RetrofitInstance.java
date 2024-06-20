9ff36bb10fb6ff717c8653bbbf7f146041839bc2
Lonrot <angelschmal.1a@gmail.com>
2024-06-20 11:48:10 +0100

    Fix: added GSON to convert from JSON to POJO

--- app/src/main/java/com/example/recordshopandroidapp/service/RetrofitInstance.java
+++ app/src/main/java/com/example/recordshopandroidapp/service/RetrofitInstance.java
@@ -1,15 +1,18 @@
 package com.example.recordshopandroidapp.service;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
-    private static final String BASE_URL = "http://127.0.0.1:8080/api/v1/album";
+    private static final String BASE_URL = "http://127.0.0.1:8080/api/v1/";
 
     public static AlbumAPIService getService() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL).build();
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
         }
         return retrofit.create(AlbumAPIService.class);
     }
