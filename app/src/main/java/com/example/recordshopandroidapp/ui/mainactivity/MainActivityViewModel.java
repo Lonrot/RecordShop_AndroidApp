a4e50b760b5e4c0a921d1ab42713968e3f2ae195
Lonrot <angelschmal.1a@gmail.com>
2024-06-20 15:16:05 +0100

    Feature: bridge between UI and data created

---
+++ app/src/main/java/com/example/recordshopandroidapp/ui/mainactivity/MainActivityViewModel.java
@@ -0,0 +1,28 @@
+package com.example.recordshopandroidapp.ui.mainactivity;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+import com.example.recordshopandroidapp.model.Album;
+import com.example.recordshopandroidapp.model.AlbumRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    AlbumRepository albumRepository;
+
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.albumRepository = new AlbumRepository(application);
+
+
+    }
+
+    public MutableLiveData<List<Album>> getAlbums() {
+        return albumRepository.getAlbumsMutableLiveData();
+    }
+
+
+}
