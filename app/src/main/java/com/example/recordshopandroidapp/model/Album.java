5e57db955ca29cb646106373a79393538267c127
Lonrot <angelschmal.1a@gmail.com>
2024-06-20 11:10:13 +0100

    Fea: Added serialization for Album

--- app/src/main/java/com/example/recordshopandroidapp/model/Album.java
+++ app/src/main/java/com/example/recordshopandroidapp/model/Album.java
@@ -1,29 +1,38 @@
 package com.example.recordshopandroidapp.model;
 
 
-
+import com.google.gson.annotations.SerializedName;
 
 public class Album {
-    private Long id;
-    private String name;
-    private String artist;
-    private String releaseYear;
-    private String genre;
-    private String label;
-    private double price;
-    private int stockQuantity;
+
+    @SerializedName("id")
+    private Long albumID;
+    @SerializedName("name")
+    private String albumName;
+    @SerializedName("artist")
+    private String albumArtist;
+    @SerializedName("releaseYear")
+    private String albumReleaseYear;
+    @SerializedName("genre")
+    private String albumGenre;
+    @SerializedName("label")
+    private String albumLabel;
+    @SerializedName("price")
+    private double albumPrice;
+    @SerializedName("stockQuantity")
+    private int albumStock;
 
 
     public Album(){}
 
-    public Album(Long id ,String name, String artist, String releaseYear, String genre, String label, double price, int stockQuantity) {
-        this.id=id;
-        this.name = name;
-        this.artist = artist;
-        this.releaseYear = releaseYear;
-        this.genre = genre;
-        this.label = label;
-        this.price = price;
-        this.stockQuantity = stockQuantity;
+    public Album(Long albumID, String albumName, String albumArtist, String albumReleaseYear, String genre, String label, double price, int stockQuantity) {
+        this.albumID = albumID;
+        this.albumName = albumName;
+        this.albumArtist = albumArtist;
+        this.albumReleaseYear = albumReleaseYear;
+        this.albumGenre = genre;
+        this.albumLabel = label;
+        this.albumPrice = price;
+        this.albumStock = stockQuantity;
     }
 }
