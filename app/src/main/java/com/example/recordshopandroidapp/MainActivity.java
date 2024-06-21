afa8e60c29de4fc4e4eaf835cc651d03c7067379
Lonrot <angelschmal.1a@gmail.com>
2024-06-21 13:59:12 +0100

    Fea: Added logic for the recycler view and app view

--- app/src/main/java/com/example/recordshopandroidapp/MainActivity.java
+++ app/src/main/java/com/example/recordshopandroidapp/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.recordshopandroidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.recordshopandroidapp.databinding.ActivityMainBinding;
+import com.example.recordshopandroidapp.model.Album;
+import com.example.recordshopandroidapp.ui.mainactivity.MainActivityViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<Album> albums;
+    private AlbumAdapter albumAdapter;
+    private MainActivityViewModel mainActivityViewModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Initialize ActivityMainBinding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        // Initialize MainActivityViewModel
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        // Initialize the album list
+        albums = new ArrayList<>();
+        // Call method to get all albums
+        getAllAlbums();
+
+    }
+
+    private void getAllAlbums() {
+        mainActivityViewModel.getAlbums().observe(this, new Observer<List<Album>>() {
+            @Override
+            public void onChanged(List<Album> albumsFromLiveData) {
+                // albums refers to the variable name of your List of Album objects
+                // albumsFromLiveData is cast to this type
+                albums = (ArrayList<Album>) albumsFromLiveData;
+
+                // This method will be created next
+                displayInRecyclerView();
+            }
+        });
+    }
+
+    private void displayInRecyclerView() {
+        // Assign RecyclerView variable to binding.recyclerView
+        recyclerView = binding.recyclerView;
+
+        // Assign AlbumAdapter variable to a new AlbumAdapter
+        albumAdapter = new AlbumAdapter(albums);
+
+        // Set the RecyclerView adapter
+        recyclerView.setAdapter(albumAdapter);
+
+        // Create and set a new LinearLayoutManager
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Set RecyclerView's setHasFixedSize to true
+        recyclerView.setHasFixedSize(true);
+
+        // Notify the adapter that the data set has changed
+        albumAdapter.notifyDataSetChanged();
     }
 }
