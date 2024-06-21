b7fb86e56bc9c0fc4d8163e1a9a5af653453e5d7
Lonrot <angelschmal.1a@gmail.com>
2024-06-21 14:48:00 +0100

    Fea: Post method added to the Service layer

--- app/src/main/java/com/example/recordshopandroidapp/service/AlbumAPIService.java
+++ app/src/main/java/com/example/recordshopandroidapp/service/AlbumAPIService.java
@@ -2,14 +2,18 @@ package com.example.recordshopandroidapp.service;
 
 import com.example.recordshopandroidapp.model.Album;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 
 import java.util.List;
 
 public interface AlbumAPIService {
 
-    @GET("album")
+    @GET("/album")
     Call<List<Album>> getAlbums();
+    @POST("/add")
+    Call<Album> addAlbum(@Body Album album);
 
 }
