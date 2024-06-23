845fb070c93fbb30ca77914cab16cbf7762de707
Lonrot <angelschmal.1a@gmail.com>
2024-06-23 21:27:24 +0100

    Fea: created the logic when the window is open to add a new album

--- app/src/main/java/com/example/recordshopandroidapp/ui/addalbum/AddNewAlbumActivity.java
+++ app/src/main/java/com/example/recordshopandroidapp/ui/addalbum/AddNewAlbumActivity.java
@@ -2,13 +2,29 @@ package com.example.recordshopandroidapp.ui.addalbum;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 import com.example.recordshopandroidapp.R;
+import com.example.recordshopandroidapp.databinding.ActivityAddNewAlbumBinding;
+import com.example.recordshopandroidapp.model.Album;
+import com.example.recordshopandroidapp.ui.mainactivity.MainActivityViewModel;
 
 public class AddNewAlbumActivity extends AppCompatActivity {
+    private MainActivityViewModel mainActivityViewModel;
+    private ActivityAddNewAlbumBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new_album);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_add_new_album);
+
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        Album albumInput = new Album();
+        binding.setAlbum(albumInput);
+
+        AddAlbumClickHandlers handlers = new AddAlbumClickHandlers(albumInput,this,mainActivityViewModel);
+        binding.setClickHandlers(handlers);
     }
 }
