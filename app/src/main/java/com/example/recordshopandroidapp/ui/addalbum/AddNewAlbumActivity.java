b5674ca1f3f509d800c9858b912a3aee17cd5f80
Lonrot <angelschmal.1a@gmail.com>
2024-06-23 20:41:31 +0100

    Fea: Created addNewAlbumActivity

---
+++ app/src/main/java/com/example/recordshopandroidapp/ui/addalbum/AddNewAlbumActivity.java
@@ -0,0 +1,14 @@
+package com.example.recordshopandroidapp.ui.addalbum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.recordshopandroidapp.R;
+
+public class AddNewAlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_album);
+    }
+}
