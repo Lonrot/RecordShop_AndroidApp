7b55e64ffa28a7e016d2e378cd959feb06dc7ca9
Lonrot <angelschmal.1a@gmail.com>
2024-06-24 15:27:13 +0100

    Fea: New activity to update and delete albums - Added dependency to debug HTTP reques for okhttp3

---
+++ app/src/main/java/com/example/recordshopandroidapp/ui/updatealbum/UpdateAlbum.java
@@ -0,0 +1,14 @@
+package com.example.recordshopandroidapp.ui.updatealbum;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.recordshopandroidapp.R;
+
+public class UpdateAlbum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_album);
+    }
+}
