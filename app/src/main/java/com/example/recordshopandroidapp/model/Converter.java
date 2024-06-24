9cc1c4a5cd8fc2dfcda9ae61bf8efb238c427629
Lonrot <angelschmal.1a@gmail.com>
2024-06-24 08:19:53 +0100

    Fea: Added double converter

--- app/src/main/java/com/example/recordshopandroidapp/model/Converter.java
+++ app/src/main/java/com/example/recordshopandroidapp/model/Converter.java
@@ -1,26 +1,53 @@
+// File: Converter.java
 package com.example.recordshopandroidapp.model;
 
 import android.util.Log;
+import androidx.databinding.BindingConversion;
 import androidx.databinding.InverseMethod;
 
 public class Converter {
 
     private static final String TAG = "Converter";
 
-    @InverseMethod("toInt")
+    @BindingConversion
     public static String fromInt(int value) {
         return String.valueOf(value);
     }
 
+    @BindingConversion
+    @InverseMethod("fromInt")
     public static int toInt(String value) {
         if (value == null) {
             return 0;
         }
         try {
             return Integer.parseInt(value);
+
         } catch (NumberFormatException e) {
+
             Log.e(TAG, "Error parsing integer from string: " + value, e);
             return 0;
         }
+    }
+
+    @BindingConversion
+    public static String fromDouble(double value) {
+        return String.valueOf(value);
+    }
+    @BindingConversion
+    @InverseMethod("fromDouble")
+    public static double toDouble(String value) {
+        if (value == null) {
+            return 0;
+        }
+        try {
+            return Double.parseDouble(value);
+
+        } catch (NumberFormatException e) {
+
+            Log.e(TAG, "Error parsing integer from string: " + value, e);
+            return 0;
+        }
+
     }
 }
