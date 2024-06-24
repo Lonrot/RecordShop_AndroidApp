7a169c054109b9650f3c7addf294e25983e33afc
Lonrot <angelschmal.1a@gmail.com>
2024-06-24 15:31:10 +0100

    Fix: added HTTP interceptor to display HTTP request bodies and responses on Logcat

--- app/src/main/java/com/example/recordshopandroidapp/service/RetrofitInstance.java
+++ app/src/main/java/com/example/recordshopandroidapp/service/RetrofitInstance.java
@@ -1,17 +1,26 @@
 package com.example.recordshopandroidapp.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
     private static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
+    static HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
 
     public static AlbumAPIService getService() {
         if (retrofit == null) {
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(logging);
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
                     .build();
         }
         return retrofit.create(AlbumAPIService.class);
